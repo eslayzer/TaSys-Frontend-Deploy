18144cd90ce3820da8a9e6319aa6f53636589242
eslayzer <ferjunior309@gmail.com>
2025-07-13 20:24:41 -0500

    Fix: Condicionar proxy de Vite solo para desarrollo

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-    import { defineConfig } from 'vite';
-    import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-    // https://vitejs.dev/config/
-    export default defineConfig({
-      plugins: [react()],
-      server: {
-        proxy: {
-          '/api': { // Todas las solicitudes que comiencen con /api
-            target: 'http://localhost:3001', // Se redirigirán a tu backend
-            changeOrigin: true,
-            rewrite: (path) => path.replace(/^\/api/, '/api'), // Asegura que la ruta /api se mantenga
-          },
-        },
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({ // Añadir 'mode' al parámetro de la función
+  plugins: [react()],
+  server: {
+    // Solo aplicar el proxy en desarrollo
+    proxy: mode === 'development' ? {
+      '/api': { // Todas las solicitudes que comiencen con /api
+        target: 'http://localhost:3001', // Se redirigirán a tu backend
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/api'), // Asegura que la ruta /api se mantenga
       },
-    });
-    
+    } : {}, // En producción, el proxy es un objeto vacío
+  },
+}));
